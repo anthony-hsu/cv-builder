b7d4b4220e88ced537afec5ec606513ababb2080
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-09-24 07:40:38 -0700

    Refactor everything else

--- src/components/CVForm.jsx
+++ src/components/CVForm.jsx
@@ -1,25 +1,16 @@
-import { Component } from "react";
 import General from "./General";
 import Experience from "./Experience";
 import Education from "./Education";
 
-class CVForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-  render() {
-    const {editMode} = this.props;
-    return (
-      <div className="cv-contents">
-        <General editMode={editMode} />
-        <Experience editMode={editMode} />
-        <Education editMode={editMode} />
-      </div>
-    )
-  }
-}
+const CVForm = (props) => {
+  const { editMode } = props;
+  return (
+    <div className="cv-contents">
+      <General editMode={editMode} />
+      <Experience editMode={editMode} />
+      <Education editMode={editMode} />
+    </div>
+  );
+};
 
 export default CVForm;
